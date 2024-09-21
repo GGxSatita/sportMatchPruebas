b7c5f8746ae318d66bfb7a8779e015f5ee8dfd4a
Iván Carreño <105950912+GGxSatita@users.noreply.github.com>
2024-09-21 13:03:40 -0300

    "proyecto base, con andr,ios y restricciones "

---
+++ android/app/src/main/java/app/web/sportmach/MainActivity.java
@@ -0,0 +1,5 @@
+package app.web.sportmach;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'sportMatch',
+  appId: 'app.web.sportmach',
+  appName: 'Sport Match',
   webDir: 'www'
 };
 
